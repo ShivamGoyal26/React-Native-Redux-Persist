70b89447c064cd63847beb66ecdef9706d7ce9db
Daisy <daisy@Daisys-iMac.local>
2021-11-12 18:09:23 +0530

    IOS Native Splash Done

--- App.js
+++ App.js
@@ -1,30 +1,26 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StatusBar} from 'react-native';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistor, store} from './Redux/Store';
 import Home from './Screens/Home';
+import SplashScreen from 'react-native-splash-screen';
 import Splash from './Screens/Splash';
 
 const App = props => {
-  const [splash, setSplash] = useState(true);
-
   useEffect(() => {
     const timer = setTimeout(() => {
-      setSplash(false);
+      SplashScreen.hide();
     }, 2000);
     return () => {
       clearTimeout(timer);
     };
   }, []);
 
-  if (splash) {
-    return <Splash />;
-  }
-
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <StatusBar barStyle="dark-content" />
         <Home />
       </PersistGate>
     </Provider>
